c430da80f8375883d84c4959942fea9854c5be5c
keroroxzz <fumincha763@gmail.com>
2025-09-14 21:58:21 +0800

    [DEV] Policy Gradient Impl

--- README.md
+++ README.md
@@ -1,38 +1,74 @@
 # Reinforcement Learning Playground
 
-This is a playground to implement Reinforcement Algorithms for personal interest.
+This is a playground to learn and implement Reinforcement Algorithms.
 
 ## Installation
 
-    # for cuda 12.1
-    conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+1. Create conda environment (Recommend)
 
-    # gym
-    conda install -c conda-forge gym
-    conda install -c conda-forge box2d-py
-    pip install pygame
+        conda create -n rlpg python==3.12
+
+
+2. Install dependency for jupyter notebook
+
+        pip install ipykernel matplotlib pyyaml typeguard
+
+3. Install gymnasium
+
+        pip install swig
+        pip install gymnasium[box2d]
+
+4. Install Pytorch
+
+    please refer to the official [Pytorch Installation Guide](https://pytorch.org/get-started/locally/)
+
+5. Install Tesorboad
+
+        pip install tensorboard
+
+### Trouble Shooting
+
+1. "Box2D is not installed"
+
+    First, verify the installation of Box2D.
+
+        python3 -c "import Box2D"
+
+    Box2D is failed to import due to mismatch version of libstdc++
+
+        ImportError: /home/rtu/miniconda3/envs/rlpg/bin/../lib/libstdc++.so.6: version `GLIBCXX_3.4.32' not found
+
+    Try updating your libstdc++
+
+        conda install -c conda-forge libgcc=15.1.0
 
 ## RL Algorithms
 
 ### Done
+1. [PolicyGradient](https://github.com/keroroxzz/RLPlayground/blob/main/framework/PolicyGradient.py)
+2. [Advantage Actro-Critic (A2C)](https://github.com/keroroxzz/RLPlayground/blob/main/framework/A2C.py)
 
-1. PolicyGradient
-2. AdvantageActroCritic (A2C)
-
-### To-be Done
-
+### To-do
 1. QLearning
 2. DeepQLearning (DQN)
 4. ProximalPolicyOptimization (PPO)
 
+## How to train your agent?
+
+### 
+
 ## Basic Classes
 
-### GymTrainer
+### 1. GymTrainer [(link)](https://github.com/keroroxzz/RLPlayground/blob/main/sim/GymTrainer.py)
 
-A class, GymTrainer, warps the gym simulation environment to train a given agent.
+- A class, GymTrainer, warps the gym simulation environment to train/evaluate a given agent.
 
-It accepts an agent implemented based on the BaseAgent.
+- It accepts an agent inheriting the BaseAgent class.
 
-### BaseAgent
+### 2. BaseAgent [(link)](https://github.com/keroroxzz/RLPlayground/blob/main/framework/BaseAgent.py)
 
-This class defines the basic RL agent behaviors and abstract functions to be implemented for different RL algorithms.
+- This class defines the basic RL agent behaviors and abstract functions to be implemented for different RL algorithms.
+
+## Reference
+
+1. Hung-yi Lee, "DRL Lecture," https://www.youtube.com/watch?v=z95ZYgPgXOY&list=PLJV_el3uVTsODxQFgzMzPLa16h6B8kWM_&ab_channel=Hung-yiLee, 2018.
--- framework/PolicyGradient.py
+++ framework/PolicyGradient.py
@@ -1,109 +1,146 @@
-
 import numpy as np
+
+# torch libs
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
-from framework.BaseAgent import BaseAgent
+
+# local libs
+from utils import *
+from framework.BaseAgent import BaseAgent, ActionData
 
 class PolicyNetwork(nn.Module):
     """
-    A Simple Policy Network to estimate the policy of the agent given a state.
-    """
+    A Policy Network outputs a probability distribution over possible actions given a state (observation).
+    It maps a state to the action probabilities by non-linear transformations, which effectively determins what action should be taken under a certain situation. 
+    For example, if an agent is approaching a wall, the policy network should assign a higher probability to the action, "decelerate", rather than "accelerate". 
 
-    def __init__(self, actionSpace):
+    ex: State:  [0.2, 5.0], distances to the wall ahead and behind, respectively
+        Output: [0.1, 0.9], 10% probability to accelerate, 90% probability to decelerate
+    """
+    def __init__(self, stateNum, actionNum, layerSize):
+        """
+        stateNum: The number of variables in the state (observation)
+        actionNum: The number of possible action
+        layer: The number of variables in the state (observation)
+        """
         super().__init__()
 
-        self.fc1 = nn.Linear(8, 16)
-        self.fc2 = nn.Linear(16, 16)
-        self.fc3 = nn.Linear(16, actionSpace)
+        # Initialize a 3 layers network
+        self.fc1 = nn.Linear(stateNum, layerSize[0])
+        self.fc2 = nn.Linear(layerSize[0], layerSize[1])
+        self.fc3 = nn.Linear(layerSize[1], actionNum)
 
     def forward(self, state):
+        # Mapping the state to logits, i.e, the "score" for each action.
         hid = torch.tanh(self.fc1(state))
-        hid = torch.tanh(hid)
-        return F.softmax(self.fc3(hid), dim=-1)
+        hid = torch.tanh(self.fc2(hid))
+        logits = self.fc3(hid)
+
+        # Finally, use the softmax function to convert scores (logits) into probabilities.
+        # Softmax(logits) = exp(logits) / sum(exp(logits))
+        # The softmax output has two important properties:
+        # 1. The sum of all probabilities equals 1.
+        # 2. Each probability lies in the range [0, 1].
+        # These properties make the output a valid probability distribution.
+        return F.softmax(logits, dim=-1)
     
 
-class PolicyGradientAgentMC(BaseAgent):
+class PolicyGradientAgent(BaseAgent):
     """
     A Policy Gradient Agent that uses Monte Carlo method to estimate the policy gradient.
     """
 
-    def __init__(self, actionSpace, gamma=0.99, lr=0.001):
-        super().__init__(actionSpace)
+    def __init__(self, actionNum, stateNum, gamma=0.99, policyLR=0.001, layerActor=[8, 8]):
+        super().__init__(actionNum)
         
         self.gamma = gamma
 
-        self.policy = PolicyNetwork(actionSpace)
-        self.optimizer = optim.SGD(self.policy.parameters(), lr=lr)
+        # Init the policy and critic networks
+        self.policy = PolicyNetwork(stateNum, actionNum, layerActor)
 
-        self.logProbs = []
-        self.reward = []
+        # Init the optimizer for our networks
+        # Choose Adam to adapt to high variance loss in RL training
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=policyLR)
+
+        # memory for training
+        self.states = []
+        self.rewards = []
+        self.dones = []
+        self.actions = []
         self.finalReward = []
 
-    def act(self, state: torch.Tensor, stage: dict = None) -> torch.Tensor:
-        actionProb = self.policy(state)
-        actionDist = Categorical(actionProb)
+    def act(self, state: torch.Tensor, stage: Stage) -> ActionData:
+        logProb = self.policy(state)
+
+        # sample an action from the probability distribution over possible actinos
+        actionDist = Categorical(logProb)
         action = actionDist.sample()
-        logProb = actionDist.log_prob(action)
 
-        self.lastAction = {"action": action, "logProb": logProb, "actionProb": actionProb}
-
-        return self.lastAction
+        return ActionData(action = action)
     
     def memorize(self, 
-              state: torch.Tensor, 
-              action: torch.Tensor, 
-              reward: torch.Tensor, 
-              nextState: torch.Tensor, 
-              done: int, 
-              stage: dict)->dict:
-        """ 
-        Collect the reward and log_prob of the action.
-        """
+              states: torch.Tensor, 
+              actions: ActionData, 
+              rewards: torch.Tensor, 
+              nextStates: torch.Tensor, 
+              dones: torch.Tensor, 
+              stage: Stage)->list[GraphPoint]:
+        self.states.append(states)
+        self.actions.append(actions.action)
+        self.rewards.append(rewards)
+        self.dones.append(dones)
+        self.finalReward.extend([r for r,d in zip(rewards, dones) if d==1])
 
-        self.logProbs.append(self.lastAction['logProb'])
-        self.reward.append(reward)
-        if done or stage['step'] == stage['maxStep'] - 1:
-            self.finalReward.append(reward)
-        
-        return {}
 
-    def onTrainEpisodeDone(self, stage: dict):
-        """ 
-        Learn from the collected experience.
-        This function will only run once every episode. 
-        
-        return: a dictionary of meta data {'key': value} to be recorded.
-        """
-        
-        return {}
-    
-    def onTrainBatchDone(self, stage: dict):
+        return [GraphPoint("Train/mean_step_reward", stage.total_step, rewards.mean().item())]
 
-        reward = torch.cat(self.reward, dim=0)
+    def onTrainBatchDone(self, stage: Stage):
+        # post-process memory
+        states = torch.stack(self.states)
+        actions = torch.stack(self.actions)
+        rewards = torch.stack(self.rewards)
+        dones = torch.stack(self.dones)
+        finalReward = torch.stack(self.finalReward)
 
-        avgReward = reward.sum().item()/len(self.finalReward)
-        finalReward = torch.cat(self.finalReward, dim=0).mean().item()
+        # calculate normalized discounted rewards
+        discountedRewards = rewards
+        for i in range(discountedRewards.shape[0]-2, -1, -1):
+            # Ri = ri + gamma * Ri+1
+            discountedRewards[i] += self.gamma * discountedRewards[i+1] * (1-dones[i])
+        # normalize the rewards to
+        # 1. prevent all positive or negative rewards
+        # 2. stablize the training by normalize the variance
+        discountedRewards = (discountedRewards - discountedRewards.mean()) / (torch.std(discountedRewards) + 1e-9)
 
-        # monte carlo method
-        for i in np.arange(reward.size(0)-2, -1, -1):
-            reward[i] += self.gamma * reward[i+1]
-
-        # normalize reward
-        reward = (reward - torch.mean(reward)) / (torch.std(reward) + 1e-9)
-
-        logProbs = torch.stack(self.logProbs)
+        # move to device
+        states = states.to(self.device)
+        actions = actions.to(self.device)
+        discountedRewards = discountedRewards.to(self.device)
 
         # train policy network
-        loss = (-logProbs * reward).sum()
+        logProbs = self.policy(states).gather(-1, actions.unsqueeze(-1)).squeeze(-1).log()
+        loss = (-logProbs * discountedRewards).mean()
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
-        self.logProbs = []
-        self.reward = []
+        rewards = torch.cat(self.rewards, dim=0)
+        avgReward = rewards.sum().item()/stage.episode
+        finalReward = finalReward.mean().item()
+
+        self.states = []
+        self.rewards = []
+        self.dones = []
+        self.actions = []
+        self.discounted_rewards = []
         self.finalReward = []
 
-        return {'loss': loss.item(), 'avgReward': avgReward, 'finalReward': finalReward}
+        print(f"Batch:{stage.totalBatch} \t Episode:{stage.totalEpisode} \t Loss: {loss.item():.2f} \t AvgRew: {avgReward:.2f} \t FinRew: {finalReward:.2f}")
+
+        return [GraphPoint('Train/loss', stage.totalEpisode, loss.item()),
+                GraphPoint('Train/avgReward', stage.totalEpisode, avgReward),
+                GraphPoint('Train/finalReward', stage.totalEpisode, finalReward),
+                GraphPoint('Train/lossActor', stage.totalEpisode, loss.item())]
